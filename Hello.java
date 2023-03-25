abba6765b491e27242aa5fcbc9fbe7139b63ffa0
Arpita Mitra <127628129+MitraArpita@users.noreply.github.com>
2023-03-25 17:12:47 +0000

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,7 +2,5 @@ public class Hello {
     public static void main(String args[]) {
 
       System.out.println("Hello world");
-      System.out.println("This is a checking");
-       System.out.println("This is my another checking");
     }
 }
